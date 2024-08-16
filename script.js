8acbfd8fe9c86e5f51215ae4ec249e86a19eaa94
Sandro Martins da Costa <profsandromartins@gmail.com>
2024-08-16 11:19:28 -0300

    Aprendendo JavaScript

---
+++ script.js
@@ -0,0 +1,12 @@
+// perguntando ao usuario
+let nome = prompt("Digite seu nome");
+alert("Olá "+nome+", tudo bem?");
+
+// alerta na tela
+alert("Ola Mundo!");
+
+// console do navegador
+console.log("Teste de console");
+
+// inclui um html em um elemento identificado pelo ID
+document.getElementById("demo").innerHTML = "Sandro Martins";
